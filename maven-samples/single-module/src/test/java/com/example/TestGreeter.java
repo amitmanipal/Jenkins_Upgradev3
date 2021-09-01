9fd42bc0607498a1fcdbb3b51d93aa799025ab29
amitmanipal <live2learnamit@gmail.com>
2021-09-01 20:01:17 +0000

    maven failure

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,11 +40,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
-  @Test
-  public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Money";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 }
